a10f4a6fbfe981dd74e50642259f626b6a0f960e
X250 <ooxx@xxoo.com>
2018-12-25 17:11:07 +0800

    finish transition

---
+++ web/my_api/algorithm/transition_pred.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 25 09:55:05 2018
+
+@author: ly
+"""
+
+import numpy as np
+
+
+Thred_b=np.array([1.05,1.05,1.05])
+Thred_m=np.array([1.05,1.05,1.05])
+
+
+def transition(cur,pre_good,pre_bad,threds):
+    """
+    cur:npArray [1003,3423,5435]
+    
+    pre_good:  [[432,5435,3243],
+                [3353,564,4234],
+                ...............
+                [4534,2423,4324]]
+    """
+    mean_good=np.mean(pre_good,axis=0)
+    mean_bad=np.mean(pre_bad,axis=0)
+    if len(pre_good) and len(pre_bad):
+        dist_good=np.linalg.norm(cur-mean_good)
+        dist_bad=np.linalg.norm(cur-mean_bad)
+        if dist_good<=dist_bad:
+            return 0
+        else:
+            return 1
+    elif len(pre_good):
+        concat=np.concatenate((cur.reshape((1,-1)),mean_good.reshape((1,-1)))).astype('float')
+        ratio=concat.max(axis=0)/concat.min(axis=0)
+        ratio_great_thred=np.greater(ratio,threds)
+        great=np.greater(cur,mean_good)
+        cc=np.array([great,ratio_great_thred])
+        boolean=cc.all(axis=0)
+        sum_bool=boolean.sum()
+        if sum_bool>1.5:
+            return 1
+        else:
+            return 0
+    elif len(pre_bad):
+        concat=np.concatenate((cur.reshape((1,-1)),mean_bad.reshape((1,-1)))).astype('float')
+        ratio=concat.max(axis=0)/concat.min(axis=0)
+        ratio_great_thred=np.greater(ratio,threds)
+        great=np.greater(cur,mean_bad)
+        cc=np.array([great,ratio_great_thred])
+        boolean=cc.all(axis=0)
+        sum_bool=boolean.sum()
+        if sum_bool>1.5:
+            return 1
+        else:
+            return 0
+
+def total_judge(observe,dic):
+    """
+    observe: npArray[5290-4869-6675-6447-4657-4259-5399-5276-6430-5885-7002-6974]
+    dic:   {'b_good':b_good,
+            'b_bad':b_bad,
+            'm_good':m_good,
+            'm_bad':m_bad}
+    """
+    
+    R1,_,R2,_,G1,_,G2,_,B1,_,B2,_=observe
+    cur_b=np.array([R1,G1,B1])
+    cur_m=np.array([R2,G2,B2])
+    b_good=np.array(dic['b_good'])
+    b_bad=np.array(dic['b_bad'])
+    m_good=np.array(dic['m_good'])
+    m_bad=np.array(dic['m_bad'])
+    
+    battery=transition(cur_b,b_good,b_bad,Thred_b)
+    meachine=transition(cur_m,m_good,m_bad,Thred_m)
+    if battery==0 and meachine==0:
+        total_statue=0
+    if battery==1 and meachine==0:
+        total_statue=1
+    if battery==0 and meachine==1:
+        total_statue=2
+    if battery==1 and meachine==1:
+        total_statue=3
+    return total_statue,battery,meachine,88,88
+    
+
+if __name__=='__main__':
+    cur=np.array([1003,3423,5435])
+    pre_good=np.random.randint(0,1000,(0,0))
+    pre_bad=np.random.randint(0,1000,(6,3))
+#    threds=np.array([1.1,1.08,1.14])
+    a=transition(cur,pre_good,pre_bad,Thred_b)
--- web/my_api/algorithm/views.py
+++ web/my_api/algorithm/views.py
@@ -19,6 +19,7 @@ import math
 import lr_pred
 import svm_pred
 import neural_pred
+import transition_pred
 
 from models import RGB
 
@@ -28,33 +29,65 @@ def timestamp2beijing(t):
     return time2
 
 
-def operate_db(uid):
-    select=RGB.objects.filter(Uid=uid)
+def operate_db(kwargs):
+    select=RGB.objects.filter(Uid=kwargs['Uid'],R1C=kwargs['R1C'],R2C=kwargs['R2C'],G1C=kwargs['G1C'],G2C=kwargs['G2C'])
+    if len(select)==0:
+        b_good=[[kwargs['R1C'],kwargs['G1C'],kwargs['B1C']]]
+        b_bad=[] 
+        m_good=[[kwargs['R2C'],kwargs['G2C'],kwargs['B2C']]]
+        m_bad=[]
+        return {'b_good':b_good,'b_bad':b_bad,'m_good':m_good,'m_bad':m_bad}
+        
+        
     select.all().order_by("Datetime")
-    select=select.values('R1','R1C','R2','R2C','Statue2')
+    select=select.values('R1','R1C','R2','R2C','G1','G1C','G2','G2C','B1','B1C','B2','B2C','Statue2')
     select=list(select)
+    select=select[-100:]
+
+    b_good=[]
+    b_bad=[] 
+    m_good=[]
+    m_bad=[]
+   
+    for dic in select:
+        if dic['Statue2']==0 or dic['Statue2']==2:
+            b_good.append([dic['R1'],dic['G1'],dic['B1']])
+        if dic['Statue2']==1 or dic['Statue2']==3:
+            b_bad.append([dic['R1'],dic['G1'],dic['B1']])
+        if dic['Statue2']==0 or dic['Statue2']==1:
+            m_good.append([dic['R2'],dic['G2'],dic['B2']])
+        if dic['Statue2']==2 or dic['Statue2']==3:
+            m_bad.append([dic['R2'],dic['G2'],dic['B2']])
 #    s=map(lambda x : x['R1'],select)
-    return select
+    return {'b_good':b_good,'b_bad':b_bad,'m_good':m_good,'m_bad':m_bad}
 
 @csrf_exempt
 def predict(request,param):
     zipdata=param.split('-')
-    uids=zipdata[12:]
+    uid=zipdata[12]
     zipdata=zipdata[:12]
+
     zipdata=map(lambda x:int(x),zipdata)
     zipdata_ln=map(lambda x:math.log(x),zipdata)
+
+    keys=['R1','R1C','R2','R2C','G1','G1C','G2','G2C','B1','B1C','B2','B2C','Uid']
+    mapping=dict(zip(keys,zipdata+[uid]))
     
-    sel=operate_db(uids[0])
-    print (sel)
+    
+    
+    
+    past_data=operate_db(mapping)
+    statue2,statue_battery2,statue_meachine2,confidence_battery2,confidence_meachine2=transition_pred.total_judge(zipdata,past_data)
+#    print ('=========',s)
     
     statue1,statue_battery1,statue_meachine1,confidence_battery1,confidence_meachine1=lr_pred.statue_judge(zipdata_ln)
-    statue2,statue_battery2,statue_meachine2,confidence_battery2,confidence_meachine2=svm_pred.statue_judge(zipdata)
+#    statue2,statue_battery2,statue_meachine2,confidence_battery2,confidence_meachine2=svm_pred.statue_judge(zipdata)
     statue3,confidence3=neural_pred.statue_judge(zipdata)
     
     t=int(time.time())
     date1=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
     date1=datetime.strptime(date1,"%Y-%m-%d %H:%M:%S")
-    rgb=RGB(Uid=uids[0],Timestamp=int(time.time()),Datetime=date1,
+    rgb=RGB(Uid=uid,Timestamp=int(time.time()),Datetime=date1,
             R1=zipdata[0],R1C=zipdata[1],R2=zipdata[2],R2C=zipdata[3],
             G1=zipdata[4],G1C=zipdata[5],G2=zipdata[6],G2C=zipdata[7],
             B1=zipdata[8],B1C=zipdata[9],B2=zipdata[10],B2C=zipdata[11],
