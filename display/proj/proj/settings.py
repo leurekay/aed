ca10fc9e6f1a5e6266118273949e843d7a30a08e
X250 <ooxx@xxoo.com>
2019-01-22 13:45:45 +0800

    add readme

--- display/proj/proj/settings.py
+++ display/proj/proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l)dqp0f$1^(7ajao+=j7jq*y^fun@9l31_%8i6q0gb63il_76z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
